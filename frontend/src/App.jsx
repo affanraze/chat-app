e68d4ae951d05e99a619aa78d2b9837134bbd794
affan <affanraze@gmail.com>
2026-08-15 20:14:57 +0500

    protected route created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import SignupPage from "./pages/SignupPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import ChatApp from "./pages/ChatApp.jsx";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
+import ProtectedRoutes from "./utils/ProtectedRout.jsx";
 
 function App() {
   return (
@@ -11,14 +12,16 @@ function App() {
       <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
-      <Route
-        path="/chat"
-        element={
-          <ThemeProvider>
-            <ChatApp />
-          </ThemeProvider>
-        }
-      />
+      <Route element={<ProtectedRoutes />}>
+        <Route
+          path="/chat"
+          element={
+            <ThemeProvider>
+              <ChatApp />
+            </ThemeProvider>
+          }
+        />
+      </Route>
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
